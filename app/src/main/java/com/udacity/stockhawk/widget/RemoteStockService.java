5216065c313dfa7b7a8d67ac82697df4399abaec
Sean <geeks.liu@gmail.com>
2017-04-17 17:40:05 +0800

    new shit

--- app/src/main/java/com/udacity/stockhawk/widget/RemoteStockService.java
+++ app/src/main/java/com/udacity/stockhawk/widget/RemoteStockService.java
@@ -1,12 +1,9 @@
 package com.udacity.stockhawk.widget;
 
-import android.content.Intent;
-import android.widget.RemoteViewsService;
-
-public class RemoteStockService extends RemoteViewsService{
-
-    @Override
-    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+//public class RemoteStockService extends RemoteViewsService{
+//
+//    @Override
+//    public RemoteViewsFactory onGetViewFactory(Intent intent) {
 //        return new RemoteStockService(this.getApplicationContext(), intent);
-    }
-}
+//    }
+//}
